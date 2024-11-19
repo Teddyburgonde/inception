8285e6503a3bf1c2202c51e9637a360406c313bf
teddy <teddy@Inception.myguest.virtualbox.org>
2024-11-19 11:26:41 +0100

    fixed many bug , but no connection with database

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -5,8 +5,8 @@ mkdir -p /run/php
 
 # Attendre que MariaDB soit disponible
 echo "Waiting for MariaDB to be available..."
-for i in {1..30}; do
-    if mysqladmin ping -h mariadb --silent; then
+for i in {1..60}; do
+    if mysqladmin ping -h ${WORDPRESS_DB_HOST} --silent; then
         echo "MariaDB is up."
         break
     else
@@ -17,22 +17,41 @@ done
 
 cd /var/www/html
 
-# Supprimer les anciens fichiers WordPress si présents
+# Supprimer les anciens fichiers WordPress s'ils sont présents
 if [ -f wp-config.php ]; then
     echo "Removing existing WordPress files..."
-    rm -rf wp-admin wp-includes wp-config.php wp-content
+    rm -rf wp-admin wp-content wp-includes wp-config.php
 fi
 
-# Télécharger WordPress si nécessaire
+# Télécharger WordPress
 if [ ! -f /usr/local/bin/wp ]; then
+    echo "Downloading WP-CLI..."
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
 fi
 
+echo "Downloading WordPress core files..."
 wp core download --allow-root
-wp config create --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${WORDPRESS_DB_HOST} --allow-root
-wp core install --url=${WORDPRESS_URL} --title="${TITLE}" --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PWD} --admin_email=${WORDPRESS_ADMIN_EMAIL} --allow-root
+
+echo "Generating wp-config.php..."
+wp config create \
+    --dbname=${WORDPRESS_DB_NAME} \
+    --dbuser=${WORDPRESS_DB_USER} \
+    --dbpass=${WORDPRESS_DB_PASSWORD} \
+    --dbhost=${WORDPRESS_DB_HOST} \
+    --allow-root
+
+echo "Installing WordPress..."
+wp core install \
+    --url=${WORDPRESS_URL} \
+    --title="${TITLE}" \
+    --admin_user=${WORDPRESS_ADMIN_USER} \
+    --admin_password=${WORDPRESS_ADMIN_PWD} \
+    --admin_email=${WORDPRESS_ADMIN_EMAIL} \
+    --allow-root
+
+echo "Creating additional WordPress user..."
 wp user create ${WORDPRESS_USER} ${WORDPRESS_EMAIL} --role=subscriber --user_pass=${WORDPRESS_PASSWORD} --allow-root
 
 chmod -R 775 wp-content
